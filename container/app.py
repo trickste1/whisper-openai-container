c1e06314e9c013ef68a1c96507f1cb5c22b57adf
tr1ckste <nikitavasilinenko@gmail.com>
2025-02-11 18:59:54 +0200

    made changes

--- container/app.py
+++ container/app.py
@@ -6,49 +6,53 @@ import urllib.parse
 import whisper
 import torch
 import boto3
+import warnings
 
 
 s3 = boto3.client("s3")
-
+bucket = "explainer-create-films-dev"
 
 def handler(event, context):
     try:
         #print("Received event: " + json.dumps(event, indent=2))
-        bucket = event["Records"][0]["s3"]["bucket"]["name"]
-        key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
-        #print("Bucket:", bucket, "key:", key)
+        print(event['Records'])
+        body = urllib.parse.unquote_plus(event['Records'][0]['body'], encoding='utf-8')
+        s3key = body.s3key
+        print("key:", s3key)
         os.makedirs("/tmp/data", exist_ok=True)
         os.chdir('/tmp/data')
 
-        audio_file=f"/tmp/data/{key}"
+        file_to_transcribe=f"/tmp/data/{s3key}"
         # Downloading file to transcribe
-        s3.download_file(bucket, key, audio_file)
+        s3.download_file(bucket, s3key, file_to_transcribe)
 
         # GPU!! (if available)
         device = "cuda" if torch.cuda.is_available() else "cpu"
-        model = whisper.load_model("medium", download_root="/usr/local").to(device)
+        model = whisper.load_model("base", download_root="/usr/local").to(device)
         #model = whisper.load_model("medium")
-        #result = model.transcribe(audio_file, fp16=False, language='English', verbose=True)
-        result = model.transcribe(audio_file, fp16=False, language='English')
+        #result = model.transcribe(file_to_transcribe, fp16=False, language='English', verbose=True)
+        result = model.transcribe(file_to_transcribe, fp16=False, verbose=True)
+        print(result['text'])
         #print(s['text'].strip())
+        object = s3.put_object(Bucket=bucket, Key=s3key+'.srt', Body=result["text"].strip())
 
-        object = s3.put_object(Bucket=bucket, Key=key+'.text', Body=result["text"].strip())
+        # try:
+        #     # Generate a pre-signed URL for the S3 object
+        #     expiration = 3600  # URL expiration time in seconds
+        #     response = s3.generate_presigned_url(
+        #         'get_object',
+        #         Params={'Bucket': bucket, 'Key': s3key+'.text'},
+        #         ExpiresIn=expiration
+        #     )
 
-        try:
-            # Generate a pre-signed URL for the S3 object
-            expiration = 3600  # URL expiration time in seconds
-            response = s3.generate_presigned_url('get_object',
-                                                    Params={'Bucket': bucket, 'Key': key+'.text'},
-                                                    ExpiresIn=expiration)
+        #     output = f"Transcribed: {key}.text - {response}"
 
-            output = f"Transcribed: {key}.text - {response}"
-
-        except ClientError as e:
-            print(e)
+        # except ClientError as e:
+        #     print(e)
 
         return {
             "statusCode": 200,
-            "body": json.dumps(output)
+            "body": json.dumps(result["text"].strip())
         }
     except Exception as e:
         print(e)
@@ -56,3 +60,5 @@ def handler(event, context):
             "statusCode": 500,
             "body": json.dumps("Error processing the file")
         }
+
+
